001a257f6d5ea8d81ed09a55c64e97162c810fd9
Divyanshu <divyanshujha02@yahoo.com>
2026-03-10 02:25:41 +0530

    Created Controller, DTO and Service class to generate Gemini Response.

--- email-writer-sb/src/main/java/com/email/email_writer/EmailWriterSbApplication.java
+++ email-writer-sb/src/main/java/com/email/email_writer/EmailWriterSbApplication.java
@@ -2,9 +2,13 @@ package com.email.email_writer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
-public class EmailWriterSbApplication {
+@ComponentScan(basePackages = "com.email.writer")
+public class
+EmailWriterSbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailWriterSbApplication.class, args);
